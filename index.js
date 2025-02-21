28bcac7a4f8bfd925682f809ae3de5099e66021b
tarun-tej-2007 <taruntej947@gmail.com>
2025-02-21 11:20:42 +0530

    connected to mongodb

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const express = require('express');
-const { resolve } = require('path');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+// Load environment variables from .env file
+dotenv.config();
 
 const app = express();
-const port = 3010;
+const port = process.env.PORT || 3000;
 
-app.use(express.static('static'));
+// MongoDB connection
+const mongoUri = process.env.MONGO_URI;
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
-});
+mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to database');
+  })
+  .catch((error) => {
+    console.error('Error connecting to database:', error);
+  });
 
+// Start the server
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Server is running on port http://localhost:${port}`);
 });
